a20ee68870e8e391f9999366550767bd6754953d
JimiKhosravi <113926772+JimiKhosravi@users.noreply.github.com>
2022-09-26 18:45:08 +0200

    Add pokemonList array
    3 objects with their features are added.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
-alert('Hello world');
-let favoriteFood = 'Pizza!';
-document.write(favoriteFood);
+let pokemonList = [{name: "Bulbasaur", height: "1", types: ['grass', 'poison']}, 
+                   {name: "Wartortle", height: "4", types: ['grass', 'poison']}, 
+                   {name: "Charmeleon", height: "7", types: ['grass', 'poison']}
+                ];
