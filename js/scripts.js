f3bf9b7b73d8e2cffdabcc5ab6dd961c32ffda53
JimiKhosravi <113926772+JimiKhosravi@users.noreply.github.com>
2022-09-27 16:53:06 +0200

    Add For loop and If statement

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,11 @@ let pokemonList = [{name: "Bulbasaur", height: "1", types: ['grass', 'poison']},
                    {name: "Wartortle", height: "4", types: ['grass', 'poison']}, 
                    {name: "Charmeleon", height: "7", types: ['grass', 'poison']}
                 ];
+
+for ( let i = 0 ; i < pokemonList.length ; i++) {
+   if (pokemonList[i].height > 4) {
+      document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big! <br>`);
+   } else {
+      document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) <br>`);
+   }
+}
