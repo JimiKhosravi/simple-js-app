81a7e4776a01ef49d737a076373a20b5910941e9
JimiKhosravi <113926772+JimiKhosravi@users.noreply.github.com>
2022-10-01 15:05:47 +0200

    Removing comments and redundant

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,3 @@
-let pokemonList = [{name: "Bulbasaur", height: "1", types: ['grass', 'poison']}, 
-                   {name: "Wartortle", height: "4", types: ['grass', 'poison']}, 
-                   {name: "Charmeleon", height: "7", types: ['grass', 'poison']}
-                ];
-
-// for ( let i = 0 ; i < pokemonList.length ; i++) {
-//    if (pokemonList[i].height > 4) {
-//       document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big! <br>`);
-//    } else {
-//       document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) <br>`);
-//    }
-// }
-
-pokemonList.forEach(function(pokemon) {
-   document.write(pokemon.name + ' ' + pokemon.height + ' ' + pokemon.types + '<br>');
-});
-
 let pokemonRepository = (function () {
    let pokemonList = [{name: "Bulbasaur", height: "1", types: ['grass', 'poison']}, 
                       {name: "Wartortle", height: "4", types: ['grass', 'poison']}, 
