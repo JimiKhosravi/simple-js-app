a73f80cd8bcb54b1d38697fb7ba596d4520822b5
JimiKhosravi <113926772+JimiKhosravi@users.noreply.github.com>
2022-10-03 20:19:51 +0200

    DOM manipulation

--- js/scripts.js
+++ js/scripts.js
@@ -13,15 +13,34 @@ let pokemonRepository = (function () {
    function getAll() {
      return pokemonList;
    }
+
+   function addListItem(pokemon){
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+    button.addEventListener('click', function(event) {
+			showDetails(pokemon);
+		})
+   }
+
+   function showDetails(pokemon){
+    console.log(pokemon);
+   }
  
    return {
      add: add,
-     getAll: getAll
+     getAll: getAll,
+     addListItem: addListItem,
+     showDetails: showDetails
    };
 })();
 
 pokemonRepository.add({name:'Beedrill', height: 5, types:['grass', 'poison']});
 
 pokemonRepository.getAll().forEach(function(pokemon){
-   document.write(pokemon.name + ' ' + pokemon.height + ' ' + pokemon.types + "<br>")
+  pokemonRepository.addListItem(pokemon);
 })
